5f2f4ca28ea26c624ce292706313a2d7f9438177
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2023-02-11 09:00:42 -0500

    Add font variables

---
+++ styles/font.ts
@@ -0,0 +1,10 @@
+import { Lato, Playfair_Display } from "@next/font/google";
+
+export const playfairDisplay = Playfair_Display({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+});
+export const lato = Lato({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
